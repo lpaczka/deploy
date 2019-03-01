e4a1409e2a6aaf049ebf1c7dc201721c831fed95
Gerardo Arjona <gerardo.arj15@gmail.com>
2019-02-28 21:09:57 -0600

    Fix Redundant import utils/index <-> userActions
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- app/actions/userActions.js
+++ app/actions/userActions.js
@@ -1,43 +1,33 @@
 const User = require("../models/Users");
-const { hashPassword } = require("../utils");
-
+const brcypt = require("bcrypt");
 const SALT_FACTOR = 10;
-
 const createUser = (data) => {
     return User.create(data);
-}
-
+};
 const getUserByEmail = (email) => {
-    return User.findOne({email:email})
-}
-
+    return User.findOne({email:email});
+};
 const addPostToUser = (id, post) => {
     return User.findByIdAndUpdate(id, {$push: {posts:post}}, {new:true});
-}
-
+};
 const getUserById = (id) => {
     return User.findOne({_id: id, is_active: true}).select("-password").populate("posts");
-}
-
+};
 const getAllUsers = () => {
     return User.find({is_active:true}).select("-password").populate("posts");
-}
-
+};
 const deleteUserById = (id) => {
-    return User.findByIdAndUpdate({_id:id, is_active:true}, {$set:{is_active:false}}, {new:true} )
-}
-
+    return User.findByIdAndUpdate({_id:id, is_active:true}, {$set:{is_active:false}}, {new:true});
+};
 const updateUserById = (id, data) => {
-    return hashPassword(data.password, SALT_FACTOR)
+    return  brcypt.hash(data.password, SALT_FACTOR)
         .then(hash => {
-            data.password = hash
-            return User.findByIdAndUpdate(id, {$set: data}, {new: true}).select("-password")
+            data.password = hash;
+            return User.findByIdAndUpdate(id, {$set: data}, {new: true}).select("-password");
         })
-        .catch(e => e)
-}
-
-module.exports ={
-
+        .catch(e => e);
+};
+module.exports = {
     createUser,
     getUserByEmail,    
     addPostToUser,
@@ -45,5 +35,4 @@ module.exports ={
     getAllUsers,
     deleteUserById,
     updateUserById
-
-}
+};
